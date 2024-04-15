ffcdaa04122c9c6eeaeeb1122ac6501bd4afbaf3
Bae Jae Min <128075457+JJamJaemin@users.noreply.github.com>
2024-04-15 23:11:26 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = []
+    
+    a = 2
+    while a <= n:
+        if n % a == 0:
+            if a not in answer:
+                answer.append(a)
+            n //= a
+        else:
+            a += 1
+    return answer
